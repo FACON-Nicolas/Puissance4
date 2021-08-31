b9bd81b91ca2da659887494762c2e8bcea15e332
FACON-Nicolas <84249866+FACON-Nicolas@users.noreply.github.com>
2021-08-31 10:08:20 +0200

    Update Puissance4.py

--- Puissance4/Puissance4.py
+++ Puissance4/Puissance4.py
@@ -1,22 +1,26 @@
-import sys
-import pygame
-import random
+import sys, pygame, random, os
+from math import sqrt as racineCarre
 from pygame.locals import *
 from joueur import Joueur
 
 pygame.init()
 
-def Create_Platform():
-    # print platform
-    for x in range(0,700,100):
-        for y in range(100,700,100):
-            surface.blit(square, (x,y))
+def draw_circle(couleur, nom, fond):
+    surface = pygame.display.set_mode((100,100))
+    surface.fill(fond)
+    for y in range(82):
+        for x in range(82):
+            r = int(racineCarre((41-x)**2+(41-y)**2))
+            if r < 41:
+                surface.set_at((9+x,9+y), couleur)
+    pygame.image.save(surface, nom + '.png')
+    surface = pygame.display.set_mode((LONGUEUR, LARGEUR), SRCALPHA)
 
 def platform_is_full():
     for i in range(len(Grille[0])):
         if Grille[0][i] == 0: return False
     font=pygame.font.Font(pygame.font.match_font('arialblack'), 50, bold=True)
-    text = font.render("Fin de Partie",True,(230,170, 230))
+    text=font.render("Fin de Partie",True,(230,170, 230))
     return True
 
 def direction(g,case, direction):
@@ -47,20 +51,16 @@ def gagne_direction(g):
 def print_game(j):
     #update game
     surface.fill((0,0,0))
-    Create_Platform()
     for l in range(6):
         for c in range(7):
-            if Grille[l][c] != 0:
-                if Grille[l][c] == 1:
-                    coin = jaune.copy()
-                elif Grille[l][c] == 2:
-                    coin = rouge.copy()
-                surface.blit(coin, (9 + (c * 100), 109 + (l * 100)))
-    surface.blit(j.image, (j.x,j.y))
+            i = Grille[l][c]
+            coin = couleur[i].copy()
+            surface.blit(coin, ((c * 100), 100 + (l * 100)))
+    surface.blit(j.image_fond, (j.x,j.y))
 
 def place_coin(j):
     #place coin in the platform
-    c = int((j.x - 9) / 100)
+    c = int((j.x) / 100)
     l = 0
     while True:
         if Grille[l+1][c] == 0 and (l+1) < 5: l += 1
@@ -86,16 +86,26 @@ Grille = [[0,0,0,0,0,0,0],
           [0,0,0,0,0,0,0]]
 
 # display initialization
-surface = pygame.display.set_mode((LONGUEUR, LARGEUR))
+surface = pygame.display.set_mode((LONGUEUR, LARGEUR), SRCALPHA)
 title = pygame.display.set_caption("Puissance 4")
 icon = pygame.image.load('P4.png')
 pygame.display.set_icon(icon)
 pygame.mouse.set_visible(False)
 
+draw_circle((255,0,0,0), 'rouge',(0,0,255))
+draw_circle((255,255,0,0), 'jaune',(0,0,255))
+draw_circle((255,0,0,0), 'rouge_fond',(0,0,0))
+draw_circle((255,255,0,0), 'jaune_fond',(0,0,0))
+draw_circle((0,0,0,0), 'noir',(0,0,255))
+
 # image initialization
-square = pygame.image.load('case.png').convert_alpha()
+j_fond = pygame.image.load('jaune_fond.png').convert_alpha()
+r_fond = pygame.image.load('rouge_fond.png').convert_alpha()
 jaune = pygame.image.load('jaune.png').convert_alpha()
 rouge = pygame.image.load('rouge.png').convert_alpha()
+noir = pygame.image.load('noir.png').convert_alpha()
+
+couleur = [noir, jaune, rouge]
 
 j1 = True
 isDown = False
@@ -103,14 +113,14 @@ win = False
 game_running = True
 clock = pygame.time.Clock()
 
-joueur1 = Joueur(jaune,1)
-joueur2 = Joueur(rouge,2)
+joueur1 = Joueur(jaune, j_fond,1)
+joueur2 = Joueur(rouge, r_fond,2)
 
 Touches = []
 
 # print game
-Create_Platform()
-surface.blit(joueur1.image, (joueur1.x, joueur1.y))
+print_game(joueur1)
+surface.blit(joueur1.image_fond, (joueur1.x, joueur1.y))
 
 # loop to keep the pygame window open
 while game_running:
@@ -126,18 +136,18 @@ while game_running:
     # player's input
     if not win:
         if j1:
-            if K_RIGHT in Touches and 609 > joueur1.x >= 9:
+            if K_RIGHT in Touches and 600 > joueur1.x >= 0:
                 if not isDown:
                     isDown = True
                     joueur1.move_coin(100, surface)
                     print_game(joueur1)
-                    joueur1.i = int((joueur1.x - 9) / 100)
-            elif K_LEFT in Touches and 609 >= joueur1.x > 9:
+                    joueur1.i = int((joueur1.x) / 100)
+            elif K_LEFT in Touches and 600 >= joueur1.x > 0:
                 if not isDown:
                     isDown = True
                     joueur1.move_coin(-100, surface)
                     print_game(joueur1)
-                    joueur1.i = int((joueur1.x - 9) / 100)
+                    joueur1.i = int((joueur1.x - 0) / 100)
             elif K_SPACE in Touches and Grille[0][joueur1.i] == 0:
                 if not isDown:
                     isDown = True
@@ -153,7 +163,7 @@ while game_running:
         elif not j1:
             pygame.time.wait(1000)
             joueur2.x = random_place() 
-            joueur2.i = int((joueur2.x - 9) / 100)
+            joueur2.i = int((joueur2.x) / 100)
             if Grille[0][joueur2.i] == 0:
                 place_coin(joueur2)
                 print_game(joueur1)
@@ -167,7 +177,7 @@ while game_running:
                 isDown = True
                 win = False
                 j1 = True
-                joueur1.x = joueur2.x = 9
+                joueur1.x = joueur2.x = 0
                 Grille = [[0,0,0,0,0,0,0],
                           [0,0,0,0,0,0,0],
                           [0,0,0,0,0,0,0],
